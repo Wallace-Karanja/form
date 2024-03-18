858fe72563b7a9c6ea8f07818eec183fd42d443c
Wallace-Karanja <wallacek10@gmail.com>
2024-03-18 15:43:08 +0300

    styles.css path adjusted for admin login, logs, passreset and register php files

--- admin/passreset.php
+++ admin/passreset.php
@@ -9,7 +9,7 @@ include './Admin.php';
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="../applicant/styles.css">
     <!-- <script src="../script.js" defer></script> -->
     <title>Form</title>
 </head>
