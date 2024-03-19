ddbdcc7fb7569e3eb8f9aa9735ebe8ecfbeb9155
Wallace-Karanja <wallacek10@gmail.com>
2024-03-19 21:58:51 +0300

    created the forms for document uploads

--- applicant/upload.php
+++ applicant/upload.php
@@ -11,6 +11,13 @@
     <link rel="stylesheet" href="styles.css">
     <!-- <script src="script.js" defer></script> -->
     <title>Form</title>
+    <style>
+        .main {
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            grid-template-rows: 1fr;
+        }
+    </style>
 </head>
 
 <body>
@@ -30,7 +37,7 @@
                         <li class="links"><a href="">Contact Us</a></li>
                     </div>
                     <div>
-                        <li class="links"><a href="./admin/">Admin</a></li>
+                        <li class="links"><a href="../admin/">Admin</a></li>
                     </div>
                 </ul>
             </nav>
@@ -40,54 +47,77 @@
         <div></div>
         <main>
             <h1>Upload Documents</h1>
-            <div>
-                <!-- <form action="" method="post" id="form" enctype="multipart/form-data">
-                    <div>
-                        <label for="name">Name of Document</label>
-                        <input type="text" name="name" id="name">
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="main">
+                    <div><label for="birth_certificate">Birth Certificate</label></div>
+                    <div><input type="file" name="birth_certificate" id="birth_certificate" />
                     </div>
-                    <div>
-                        <label for="document">Document</label>
-                        <input type="file" name="document" id="document">
+                    <div><input type="submit" name="submit" value="Upload" id="submit" /></div>
+                </div>
+            </form>
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="main">
+                    <div><label for="kcse">Kenya Certificate of Secondary Education (KCSE)</label></div>
+                    <div><input type="file" name="kcse" id="kcse" />
                     </div>
-                    <div></div>
-                    <div><input type="submit" name="submit" value="upload" id="submit" /></div>
-                </form> -->
+                    <div><input type="submit" name="submit" value="Upload" id="submit" /></div>
+                </div>
+            </form>
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="main">
+                    <div><label for="kcpe">Kenya Certificate of Primary Education (KCPE)</label></div>
+                    <div><input type="file" name="kcpe" id="kcse" />
+                    </div>
+                    <div><input type="submit" name="submit" value="Upload" id="submit" /></div>
+                </div>
+            </form>
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="main">
+                    <div><label for="id">Identity Card</label></div>
+                    <div><input type="file" name="id_card" id="id" />
+                    </div>
+                    <div><input type="submit" name="submit" value="Upload" id="submit" /></div>
+                </div>
+            </form>
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="main">
+                    <div><label for="leaving_certificate">School Leaving Certificate</label></div>
+                    <div><input type="file" name="leaving_certificate" id="id" />
+                    </div>
+                    <div><input type="submit" name="submit" value="Upload" id="submit" /></div>
+                </div>
+            </form>
 
-                <form action="upload.php" method="post" enctype="multipart/form-data">
-                    <label for="document">Kenya Certificate of Secondary Education (KCSE):</label>
-                    <input type="file" id="document" name="document" accept=".pdf,.doc,.docx"><br><br>
-                    <input type="submit" value="Upload">
-                </form>
+            <p id="message"></p>
+            <?php
+            if (isset($_FILES['birth_certificate'])) {
+                $file_tmp_name = $_FILES['birth_certificate']['tmp_name'];
+                $file_name = $_FILES['birth_certificate']['name'];
+                // $fileSize = $_FILES['birth_certificate']['size'];
+                // $maxFileSize = 2 * 1024 * 1024;
+                // $uploadDir = "uploads/";
+                // $destination = $uploadDir . $file_name;
+                // if (move_uploaded_file($file_tmp_name, $destination)) {
+                //     echo "File upload successful";
+                // } else {
+                //     echo "Failed to upload file";
+                // }
+                echo $file_name;
+                echo "<br>";
+                $fileExtension = pathinfo($file_name, PATHINFO_EXTENSION);
+                $_FILES['birth_certificate']['name'] = "Firstname_Lastname_" . "birth_certificate." . $fileExtension;
+                $file_name = $_FILES['birth_certificate']['name'];
+                echo $file_name;
+                // var_dump($_FILES['birth_certificate']['name']);
+                // var_dump($file_name);
+                // var_dump($file_tmp_name); // path to file in tmp folder
+            }
 
-                <p id="message"></p>
-                <?php
-                if (isset($_FILES['document'])) {
-                    $file_tmp_name = $_FILES['document']['tmp_name'];
-                    $file_name = $_FILES['document']['name'];
-                    // echo $file_tmp_name;
-                    // echo $file_name;
-                    $fileSize = $_FILES['document']['size'];
-                    // echo $fileSize;
-                    $maxFileSize = 2 * 1024 * 1024;
-                    // echo ($fileSize < $maxFileSize ? "Okay" : "not okay");
-                    $uploadDir = "uploads/";
-                    $destination = $uploadDir . $file_name;
-                    // if (move_uploaded_file($file_tmp_name, $destination)) {
-                    //     echo "File upload successful";
-                    // } else {
-                    //     echo "Failed to upload file";
-                    // }
-
-                    var_dump($_FILES);
-                } else {
-                    echo "Error uploading file";
-                }
-
-                ?>
-            </div>
-        </main>
-        <div></div>
+            // fields birth_certificate, kcse, kcpe, id_card, leaving_certificate
+            ?>
+    </div>
+    </main>
+    <div></div>
     </div>
 
 </body>
